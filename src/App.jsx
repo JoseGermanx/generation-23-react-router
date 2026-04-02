ea6f0c95aba2803231c5c99c89396027eaea5d30
José Martínez <jgermanmm@gmail.com>
2026-04-02 11:04:03 -0300

    ruta comodín

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from './views/About'
 import Home from './views/Home';
 import User from './views/User';
 import Contact from './views/Contact';
+import NotFound from './views/NotFound';
 
 // Routes define grupo de rutas
 // Route define una Ruta
@@ -13,10 +14,15 @@ function App() {
 
   return (
     <Routes>
+      { /* Rutas  */}
       <Route path='/' element={<Home />} />
       <Route path='/user' element={<User />} />
       <Route path='/about' element={<About />} />
       <Route path='/contact' element={<Contact />} />
+
+      { /* Ruta comodín */}
+      <Route path='*' element={<NotFound />} />
+
     </Routes>
   )
 }
