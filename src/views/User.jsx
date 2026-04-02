7017edfb07cd3c3b3cfbc170a3951eb80e5cb906
José Martínez <jgermanmm@gmail.com>
2026-04-02 12:52:25 -0300

    implementacion de rutas dinámicas

--- src/views/User.jsx
+++ src/views/User.jsx
@@ -1,14 +1,32 @@
 
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router'
 
 const User = () => {
 
     const { id } = useParams(); // el id viene desde la definción de la ruta
+    const [user, setUser] = useState(null);
+
+     useEffect(()=> {
+
+        const getUser = async () => {
+            const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+            const data = await response.json();
+            setUser(data)
+        }
+        getUser();
+
+    }, [id])
+
+    if (!user) return <p>Usuario no existe!!</p>
+
 
     return (
         <>
             <h1>Información del usuario</h1>
             <h3>Usuario ID: {id}</h3>
+            <p>Nombre: {user.name}</p>
+            <p>Username: {user.username}</p>
         </>
     )
 };
