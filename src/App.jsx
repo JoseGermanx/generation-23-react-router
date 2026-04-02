43949c56b07378eda1d02ed96c0ae8a2a4d3628b
José Martínez <jgermanmm@gmail.com>
2026-04-02 10:34:42 -0300

    definicion inicial de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+
+import {Routes, Route} from 'react-router';
 import About from './views/About'
-import Contact from './views/Contact'
+
+// Routes define grupo de rutas
+// Route define una Ruta
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <Contact />
+    <Routes>
+      <Route path='/' element={<About />} />
+    </Routes>
   )
 }
 
