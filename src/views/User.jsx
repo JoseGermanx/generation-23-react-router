77fdce2b9fa8720b4afcb60f6449d92d85834ad8
José Martínez <jgermanmm@gmail.com>
2026-04-02 13:03:00 -0300

    gestion de error de usuario no existente

--- src/views/User.jsx
+++ src/views/User.jsx
@@ -6,27 +6,43 @@ const User = () => {
 
     const { id } = useParams(); // el id viene desde la definción de la ruta
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true)
+    const [ resultado, setResultado] = useState(true)
 
-     useEffect(()=> {
+    useEffect(() => {
 
         const getUser = async () => {
             const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+            if (!response.ok) {
+                setResultado(false)
+                setLoading(false)
+                return
+            }
+
             const data = await response.json();
+            console.log(data)
             setUser(data)
+            setLoading(false)
         }
         getUser();
 
     }, [id])
 
-    if (!user) return <p>Usuario no existe!!</p>
+    if (loading) return <p>Cargando....</p>
 
-
+   
     return (
         <>
-            <h1>Información del usuario</h1>
-            <h3>Usuario ID: {id}</h3>
-            <p>Nombre: {user.name}</p>
-            <p>Username: {user.username}</p>
+            {!resultado ? (<p>Usuario no existe!!</p>) :
+                (
+                    <>
+                        <h1>Información del usuario</h1>
+                        <h3>Usuario ID: {id}</h3>
+                        <p>Nombre: {user.name}</p>
+                        <p>Username: {user.username}</p>
+                    </>
+                )
+            }
         </>
     )
 };
